19625d8ae4aa0d68682f5866d20ada9d87cf5b93
Kevin Bost <kevin@intellitect.com>
2016-06-08 10:52:22 -0700

    Adding brain teaser problem

---
+++ MutatingStrings/MutatingStrings/Tests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MutatingStrings
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void MutateTheImmutable()
+        {
+            const string WhoAmI = "Kevin";
+            Assert.AreEqual("Kevin", WhoAmI);
+
+            CauseIdentityCrisis(WhoAmI);
+
+            Assert.AreEqual("Grant", WhoAmI);
+        }
+
+        private static void CauseIdentityCrisis( string whoAmI )
+        {
+            //TODO
+        }
+
+        [TestMethod]
+        public void MutateTheUnnamed()
+        {
+            const string WhoAmI = "Kelly";
+            Assert.AreEqual("Kelly", WhoAmI);
+
+            CauseIdentityCrisis();
+
+            Assert.AreEqual("Chris", WhoAmI);
+        }
+
+        private static void CauseIdentityCrisis()
+        {
+            //TODO
+        }
+    }
+}
